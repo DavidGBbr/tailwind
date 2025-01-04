f93d99d4b7683cb112f0714a5a750e22202050a1
DavidGBbr <davidgomesbrigido@gmail.com>
2025-01-04 11:41:56 -0300

    feat: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"DM Sans"', "system-ui"],
+        serif: ['"DM Serif Text"', "Georgia"],
+      },
+      colors: {
+        verde: {
+          200: "#acef75",
+          300: "#91EE77",
+          900: "#16281F",
+          950: "#0F1C15",
+        },
+        green: {
+          300: "#91EE77",
+        },
+      },
+      spacing: {
+        22: "22rem",
+      },
+      fontSize: {
+        xl: ["1.25rem", { lineHeight: "1.5rem" }],
+        "2xs": ".5rem",
+      },
+      gridTemplateColumns: {
+        "1/2": "1fr 2fr",
+      },
+      screens: {
+        xs: "380px",
+      },
+    },
   },
   plugins: [],
 };
