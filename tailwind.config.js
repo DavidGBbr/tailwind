891e4b8350787b7fcdeb36f3f4cf7dcd278efab9
DavidGBbr <davidgomesbrigido@gmail.com>
2025-01-04 11:57:14 -0300

    feat: adicionar estilo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,50 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+import plugin from "tailwindcss/plugin";
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    colors: ({ colors }) => ({
-      lime: colors.lime,
-      white: "white",
+  theme: {},
+  plugins: [
+    plugin(function ({ addUtilities, addComponents }) {
+      addUtilities({
+        ".text-shadow": {
+          "text-shadow": "1px 1px currentColor",
+        },
+        ".text-shadow-lg": {
+          "text-shadow": "2px 2px currentColor",
+        },
+      });
+      addComponents({
+        ".btn-ghost": {
+          "border-radius": "9999px",
+          padding: ".5rem 1rem",
+          opacity: ".2",
+          border: "2px solid #ccc",
+        },
+      });
     }),
-    extend: {
-      fontFamily: {
-        sans: ['"DM Sans"', "system-ui"],
-        serif: ['"DM Serif Text"', "Georgia"],
-      },
-      colors: {
-        verde: {
-          200: "#acef75",
-          300: "#91EE77",
-          900: "#16281F",
-          950: "#0F1C15",
-        },
-        green: {
-          300: "#91EE77",
-        },
-      },
-      spacing: {
-        22: "22rem",
-      },
-      fontSize: {
-        xl: ["1.25rem", { lineHeight: "1.5rem" }],
-        "2xs": ".5rem",
-      },
-      gridTemplateColumns: {
-        "1/2": "1fr 2fr",
-      },
-      screens: {
-        xs: "380px",
-      },
-      keyframes: {
-        slideIn: {
-          "0%": { opacity: 0, transform: "translateX(-20%)" },
-          "100%": { opacity: 1, transform: "translateX(0)" },
-        },
-      },
-      animation: {
-        slideIn: "slideIn .3s ease-in-out forwards",
-      },
-    },
-  },
-  plugins: [],
+  ],
 };
