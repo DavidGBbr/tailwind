8546254c180e3495e7193fd23a68e52ee979a5e8
DavidGBbr <davidgomesbrigido@gmail.com>
2025-01-04 12:02:43 -0300

    feat: container

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-import plugin from "tailwindcss/plugin";
-
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {},
-  plugins: [
-    plugin(function ({ addUtilities, addComponents }) {
-      addUtilities({
-        ".text-shadow": {
-          "text-shadow": "1px 1px currentColor",
-        },
-        ".text-shadow-lg": {
-          "text-shadow": "2px 2px currentColor",
-        },
-      });
-      addComponents({
-        ".btn-ghost": {
-          "border-radius": "9999px",
-          padding: ".5rem 1rem",
-          opacity: ".2",
-          border: "2px solid #ccc",
-        },
-      });
-    }),
-  ],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: ".5rem",
+        sm: "1rem",
+      },
+    },
+  },
 };
